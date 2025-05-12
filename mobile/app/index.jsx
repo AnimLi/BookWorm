291aa90be37fa25d63bedc3ecbb8320355cd19fd
AnimLi <leowork430@gmail.com>
2025-05-12 12:23:13 +0800

    React native Basics & Environment setup

---
+++ mobile/app/index.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.title}>Testing</Text>
+      <Text>Welcome world</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
